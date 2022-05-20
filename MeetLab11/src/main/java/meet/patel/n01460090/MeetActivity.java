da05e7399c0899ee8ad8dcd59a1b1767dfc42715
Meet2304 <meetpatel12302@gmail.com>
2022-05-20 01:53:35 -0400

    Adding the Second Button which displays the text entered by the user in a toast.

--- MeetLab11/src/main/java/meet/patel/n01460090/MeetActivity.java
+++ MeetLab11/src/main/java/meet/patel/n01460090/MeetActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MeetActivity extends AppCompatActivity {
@@ -22,9 +23,16 @@ public class MeetActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Creating a button for the first button which launches a site
         Button siteButton;
         siteButton = findViewById(R.id.MeetSiteButton);
 
+        // Creating a button for the second button which displays a toast
+        Button displayButton;
+        displayButton = findViewById(R.id.MeetToastDisplayButton);
+
+
+        // Setting up the functioning of the First button
         siteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +53,25 @@ public class MeetActivity extends AppCompatActivity {
                 startActivity(new Intent(Intent.ACTION_VIEW, uri));
             }
 
+        });
+
+
+        // Setting up the functioning of the Second Button
+        displayButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                EditText text;
+                text = (EditText)findViewById(R.id.MeetEditText);
+                String textToToast = text.getText().toString();
+
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(MeetActivity.this, textToToast, duration);
+                toast.show();
+
+            }
         });
 
 
